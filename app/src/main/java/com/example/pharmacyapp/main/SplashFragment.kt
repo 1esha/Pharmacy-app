0ab27d3215b3073f0c75fc61eb2a14e3677d8b0f
lesha <alesha.baxtun@mail.ru>
2024-11-24 12:32:32 +0300

    добавление перехода с SplashFragment

--- app/src/main/java/com/example/pharmacyapp/main/SplashFragment.kt
+++ app/src/main/java/com/example/pharmacyapp/main/SplashFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.example.pharmacyapp.R
 import com.example.pharmacyapp.databinding.FragmentSplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
-    private val binding = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,11 +30,38 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        val navControllerMain = findNavController()
+
+        //isInit отвечает за то надо ли показывать пользователю InitFragment
+        var isInit = true
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(DEFAULT_DELAY)
+            if (isInit){
+                navControllerMain.navigate(R.id.action_splashFragment_to_initFragment,null, navOptions {
+                    popUpTo(R.id.splashFragment){
+                        inclusive = true
+                    }
+                })
+            }
+            else{
+                navControllerMain.navigate(R.id.action_splashFragment_to_tabsFragment,null, navOptions {
+                    popUpTo(R.id.splashFragment){
+                        inclusive = true
+                    }
+                })
+            }
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val DEFAULT_DELAY = 1500L
     }
 
 }
