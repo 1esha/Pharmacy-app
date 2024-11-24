83fb27d2bb0bb994280367ef50e52b1815fdb40b
lesha <alesha.baxtun@mail.ru>
2024-11-24 11:36:45 +0300

    добавление SplashFragment

---
+++ app/src/main/java/com/example/pharmacyapp/main/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.example.pharmacyapp.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pharmacyapp.R
+import com.example.pharmacyapp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
