7a8b902d536127afa44ebf030c55826eff37879b
lesha <alesha.baxtun@mail.ru>
2024-11-24 11:59:12 +0300

    добавление LoginFragment

---
+++ app/src/main/java/com/example/pharmacyapp/main/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.pharmacyapp.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pharmacyapp.R
+import com.example.pharmacyapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
