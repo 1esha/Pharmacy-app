8810817411eb5cdce34d970264677325e85e000a
lesha <alesha.baxtun@mail.ru>
2025-03-31 21:59:39 +0300

    удаление не используемых мапперов для преобразования типов из DataSource в типы для репозитория и добавлениек маппера для преобразования UserInfoDataSourceModel в UserInfoModel.

--- data/src/main/java/com/example/data/Utils.kt
+++ data/src/main/java/com/example/data/Utils.kt
@@ -8,13 +8,8 @@ import com.example.data.catalog.datasource.models.ProductAvailabilityDataSourceM
 import com.example.data.catalog.datasource.models.ProductDataSourceModel
 import com.example.data.profile.datasource.models.LogInDataSourceModel
 import com.example.data.profile.datasource.models.ResponseDataSourceModel
-import com.example.data.profile.datasource.models.ResponseValueDataSourceModel
 import com.example.data.profile.datasource.models.UserDataSourceModel
 import com.example.data.profile.datasource.models.UserInfoDataSourceModel
-import com.example.domain.ErrorResult
-import com.example.domain.PendingResult
-import com.example.domain.Result
-import com.example.domain.SuccessResult
 import com.example.domain.catalog.models.PharmacyAddressesDetailsModel
 import com.example.domain.favorite.models.FavoriteModel
 import com.example.domain.catalog.models.ProductAvailabilityModel
@@ -23,7 +18,6 @@ import com.example.domain.catalog.models.PharmacyAddressesModel
 import com.example.domain.models.OperatingModeModel
 import com.example.domain.profile.models.LogInModel
 import com.example.domain.profile.models.ResponseModel
-import com.example.domain.profile.models.ResponseValueModel
 import com.example.domain.profile.models.UserInfoModel
 import com.example.domain.profile.models.UserModel
 
@@ -31,29 +25,10 @@ import com.example.domain.profile.models.UserModel
  *  Utils.kt содержит функции
  *  для преобразования типов из DataSource в типы для репозитория
  */
-
 const val SUCCESS = "Успешно"
 const val SUCCESS_CODE = 200
 const val NOT_SELECTED = "NOT_SELECTED"
 
-
-fun <I,O> ResultDataSource<I>.toResult(value:O?):Result<O>{
-    return when(this){
-        is PendingResultDataSource<I> -> {
-            PendingResult<O>()
-        }
-        is SuccessResultDataSource<I> -> {
-            SuccessResult<O>(
-                value = value
-            )
-        }
-        is ErrorResultDataSource<I> -> {
-            ErrorResult<O>(
-                exception = this.exception
-            )
-        }
-    }
-}
 
 fun ResponseDataSourceModel.toResponseModel():ResponseModel{
     return ResponseModel(
@@ -62,9 +37,6 @@ fun ResponseDataSourceModel.toResponseModel():ResponseModel{
     )
 }
 
-fun <T> ResultDataSource<T>.asSuccessResultDataSource(): SuccessResultDataSource<T>?{
-    return if (this is SuccessResultDataSource<T>) this else null
-}
 
 // ProfileRepository
 
@@ -80,37 +52,21 @@ fun UserInfoModel.toUserInfoDataSourceModel(): UserInfoDataSourceModel {
     )
 }
 
-
-fun ResponseValueDataSourceModel<String>.toResponseValueStringModel(): ResponseValueModel<String> {
-    return ResponseValueModel(
-        value = this.value,
-        responseModel = this.responseDataSourceModel.toResponseModel()
+fun UserInfoDataSourceModel.toUserInfoModel(): UserInfoModel {
+    return UserInfoModel(
+        firstName = this.firstName,
+        lastName = this.lastName,
+        email = this.email,
+        phoneNumber = this.phoneNumber,
+        userPassword = this.userPassword,
+        city = this.city
     )
 }
 
-fun ResponseValueDataSourceModel<UserDataSourceModel>.toResponseValueUserModelModel(): ResponseValueModel<UserModel>{
-    return ResponseValueModel(
-        value = this.value?.let {
-            UserModel(
-                userId = it.userId,
-                userInfoModel = UserInfoModel(
-                    firstName = this.value.userInfoModel.firstName,
-                    lastName = this.value.userInfoModel.lastName,
-                    email = this.value.userInfoModel.email,
-                    phoneNumber = this.value.userInfoModel.phoneNumber,
-                    userPassword = this.value.userInfoModel.userPassword,
-                    city = this.value.userInfoModel.city,
-                )
-            )
-        },
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
-fun ResponseValueDataSourceModel<Int>.toResponseValueIntModel(): ResponseValueModel<Int>{
-    return ResponseValueModel(
-        value = this.value ,
-        responseModel = this.responseDataSourceModel.toResponseModel()
+fun UserDataSourceModel.toUserModel(): UserModel{
+    return UserModel(
+        userId = this.userId,
+        userInfoModel = this.userInfoModel.toUserInfoModel()
     )
 }
 
@@ -184,34 +140,6 @@ fun List<ProductAvailabilityDataSourceModel>.toListProductAvailabilityModel(): L
     return listProductAvailabilityModel
 }
 
-fun ResponseValueDataSourceModel<ProductDataSourceModel?>.toResponseValueProductModel(): ResponseValueModel<ProductModel?> {
-    return ResponseValueModel(
-        value = this.value?.toProductModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
-fun ResponseValueDataSourceModel<List<ProductDataSourceModel>?>.toResponseValueListProductModel(): ResponseValueModel<List<ProductModel>?>{
-    return ResponseValueModel(
-        value = this.value?.toListProductModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
-fun ResponseValueDataSourceModel<List<PharmacyAddressesDataSourceModel>?>.toResponseValueListPharmacyAddressesModel(): ResponseValueModel<List<PharmacyAddressesModel>?> {
-    return ResponseValueModel(
-        value = this.value?.toListPharmacyAddressesModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
-fun ResponseValueDataSourceModel<List<ProductAvailabilityDataSourceModel>?>.toResponseValueListProductAvailabilityModel(): ResponseValueModel<List<ProductAvailabilityModel>?> {
-    return ResponseValueModel(
-        value = this.value?.toListProductAvailabilityModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
 fun PharmacyAddressesDetailsDataSourceModel.toPharmacyAddressesDetailsModel(): PharmacyAddressesDetailsModel {
     return PharmacyAddressesDetailsModel(
         pharmacyAddressesModel = PharmacyAddressesModel(
@@ -239,13 +167,6 @@ fun List<PharmacyAddressesDetailsDataSourceModel>.toListPharmacyAddressesDetails
     return mutableListPharmacyAddressesDetailsModel
 }
 
-fun ResponseValueDataSourceModel<List<PharmacyAddressesDetailsDataSourceModel>?>.toResponseValueListPharmacyAddressesDetailsDataSourceModel(): ResponseValueModel<List<PharmacyAddressesDetailsModel>?> {
-    return ResponseValueModel(
-        value = this.value?.toListPharmacyAddressesDetailsModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
 fun OperatingModeDataSourceModel.toOperatingModeModel(): OperatingModeModel {
     return OperatingModeModel(
         modeId = this.modeId,
@@ -265,21 +186,7 @@ fun List<OperatingModeDataSourceModel>?.toListOperatingModeModel(): List<Operati
     return mutableListOperatingMode
 }
 
-fun ResponseValueDataSourceModel<List<OperatingModeDataSourceModel>?>.toResponseValueListOperatingModeModel(): ResponseValueModel<List<OperatingModeModel>?> {
-    return ResponseValueModel(
-        value = this.value.toListOperatingModeModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
-
 // FavoriteRepository
-
-fun ResponseValueDataSourceModel<List<FavoriteEntity>>.toResponseValueListFavoriteModel(): ResponseValueModel<List<FavoriteModel>> {
-    return ResponseValueModel(
-        value = this.value.toListFavoriteModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
-    )
-}
 
 fun List<FavoriteEntity>?.toListFavoriteModel(): List<FavoriteModel> {
     val mutableListFavoriteModel = mutableListOf<FavoriteModel>()
@@ -310,12 +217,5 @@ fun FavoriteModel.toFavoriteEntity() : FavoriteEntity {
         price = favoriteModel.price,
         discount = favoriteModel.discount,
         image = favoriteModel.image
-    )
-}
-
-fun ResponseValueDataSourceModel<FavoriteEntity>.toResponseValueFavoriteModel(): ResponseValueModel<FavoriteModel> {
-    return ResponseValueModel(
-        value = this.value?.toFavoriteModel(),
-        responseModel = this.responseDataSourceModel.toResponseModel()
     )
 }
