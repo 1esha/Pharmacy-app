0a33ce0f2b93347c1f8a7e7928787b241897a2eb
lesha <alesha.baxtun@mail.ru>
2025-06-23 20:32:06 +0300

    изменение функционала для шифрование данных, подходящий для min sdk.

--- domain/src/main/java/com/example/domain/EncryptionUtils.kt
+++ domain/src/main/java/com/example/domain/EncryptionUtils.kt
@@ -1,34 +1,46 @@
 package com.example.domain
 
-import java.util.Base64
+import org.apache.commons.codec.binary.Base64
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
+import java.nio.charset.StandardCharsets
 
 class EncryptionUtils(private val key: String = KEY) {
 
-    // Функция шифровки строки
+    /**
+     * Функция шифрует строку с использованием AES-ECB-PKCS5Padding.
+     */
     fun encrypt(text: String): String {
-        val secretKey = SecretKeySpec(key.toByteArray(), "AES")
+        val secretKey = SecretKeySpec(key.toByteArray(StandardCharsets.UTF_8), "AES")
         val cipher = Cipher.getInstance(INSTANCE_STRING)
+
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
 
-        val encryptedData = cipher.doFinal(text.toByteArray(Charsets.UTF_8))
-        return Base64.getEncoder().encodeToString(encryptedData)
-    }
+        // Шифруем данные
+        val encryptedData = cipher.doFinal(text.toByteArray(StandardCharsets.UTF_8))
 
-    // Функция дешифровки строки
+        return Base64.encodeBase64String(encryptedData)
+    }
+    /**
+     * Функция дешифрует строку.
+     */
     fun decrypt(text: String): String {
-        val decodedBytes = Base64.getDecoder().decode(text)
-        val secretKey = SecretKeySpec(key.toByteArray(), "AES")
+
+        val decodedBytes = Base64.decodeBase64(text)
+
+        val secretKey = SecretKeySpec(key.toByteArray(StandardCharsets.UTF_8), "AES")
         val cipher = Cipher.getInstance(INSTANCE_STRING)
+
         cipher.init(Cipher.DECRYPT_MODE, secretKey)
 
+        // Дешифруем данные
         val decryptedData = cipher.doFinal(decodedBytes)
-        return String(decryptedData, Charsets.UTF_8)
+        return String(decryptedData, StandardCharsets.UTF_8)
     }
 
     companion object {
         private const val INSTANCE_STRING = "AES/ECB/PKCS5Padding"
         private const val KEY = "Ed49Kor65Fn!dM823Ytr93WaVcbn1azX"
     }
+
 }
